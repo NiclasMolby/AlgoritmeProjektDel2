f124f0b530fe5b43b43188b5cec12a3427a080e7
Niclas Mølby <niclas@molby.net>
2017-03-22 13:05:05 +0100

    search

--- src/DictBinTree.java
+++ src/DictBinTree.java
@@ -5,7 +5,7 @@ public class DictBinTree implements Dict {
 
     private Node root;
     private int size;
-    private int counter;
+    private int pointer;
 
     public DictBinTree() {
 
@@ -16,12 +16,34 @@ public class DictBinTree implements Dict {
     }
 
     public int[] orderedTraversal() {
+        int[] orderedArray = new int[size];
+
+
         return new int[0];
+    }
+
+    private void inOrderTreeWalk(Node n){
+        if(n != null) {
+            inOrderTreeWalk(n.getLeftChild());
+            System.out.println(n.getKey());
+            inOrderTreeWalk(n.getRightChild());
+        }
     }
 
     public boolean search(int k) {
         Node current = root;
-        while(current != null && current.)
+        while(current != null && current.getKey() != k) {
+            if (k < current.getKey()) {
+                current = current.getLeftChild();
+            }
+            else {
+                current = current.getRightChild();
+            }
+        }
+
+        if(current.getKey() == k) {
+            return true;
+        }
 
         return false;
     }
